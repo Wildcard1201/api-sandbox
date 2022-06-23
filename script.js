9008d21c9b10eb332af82dd8371795611da97b0e
Jason Fuhrer <jf.33@hotmail.com>
2022-06-23 08:46:33 -0400

    js init

---
+++ script.js
@@ -0,0 +1,26 @@
+async function getFact() {
+    let url = 'https://cat-fact.herokuapp.com/facts'
+    try {
+        let fact = await fetch(url)
+        console.log('fetch ok')
+        return await fact.json()
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function renderFact() {
+    let facts = getFact()
+    let html = ''
+
+    console.log('made it this far')
+
+    facts.forEach((fact) => {
+        html += fact
+    })
+
+    let container = document.getElementById('container')
+    container.innerHTML = html;
+}
+
+renderFact();
