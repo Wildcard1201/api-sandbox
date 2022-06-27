5c0b1e0b7f55eb3e73186fb2c37f0e67eb0f44b2
Jason Fuhrer <jf.33@hotmail.com>
2022-06-26 21:48:14 -0400

    add url display

--- template/script.js
+++ template/script.js
@@ -1,1 +1,10 @@
 // script below
+let urlDisplay = document.getElementById('url-display')
+let container = document.getElementById('container')
+let url = 'https://www.website.com'
+
+function printUrl() {
+    urlDisplay.innerHTML = url
+}
+
+printUrl()
